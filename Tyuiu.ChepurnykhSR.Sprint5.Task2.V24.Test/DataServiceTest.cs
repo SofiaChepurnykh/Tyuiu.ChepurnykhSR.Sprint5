7ed1c0d0e1997d7c06cd83a9dd45ead61dbe6c39
Tyuiu.ChepurnykhSR@outlook.com <winxsofa@bk.ru>
2023-11-19 18:26:16 +0500

    Task2

---
+++ Tyuiu.ChepurnykhSR.Sprint5.Task2.V24.Test/DataServiceTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using System.IO;
+
+using Tyuiu.ChepurnykhSR.Sprint5.Task2.V24.Lib;
+
+namespace Tyuiu.ChepurnykhSR.Sprint5.Task2.V24.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidSaveToFileTextData()
+        {
+
+            DataService ds = new DataService();
+            string path = @"C:\Users\user\source\repos\Tyuiu.ChepurnykhSR.Sprint5\Tyuiu.ChepurnykhSR.Sprint5.Task2.V24\bin\Debug\OutPutFileTask2.csv";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+
+        }
+    }
+}
